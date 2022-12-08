04763b1037000385917c7e7f6df95940bc28a287
Chris <cr1s@hotmail.it>
2022-12-08 19:00:41 +0100

    Add: exercise_Output_3

---
+++ src/PrintYourNameAndPi.java
@@ -0,0 +1,10 @@
+public class PrintYourNameAndPi {
+    public static void main(String[] args) {
+
+
+        String name = "Chris";
+        double p = 3.141590;
+        System.out.printf("MyNane: %s  %n",name);
+        System.out.printf("The number Pi is: %f",p);
+    }
+}
